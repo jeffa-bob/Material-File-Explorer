8e0e335f8a4c6f3eac5fc7b841287aa8fd19ebdf
Tyler Brody <brody.tyler.46@gmail.com>
2020-08-14 14:51:51 -0700

    changed to file info from custrom struct

--- Materialfile.filereaders/Class1.cs
+++ Materialfile.filereaders/Class1.cs
@@ -4,47 +4,16 @@ using System.Linq;
 
 namespace Materialfile.filereader
 {
-  public struct filitem
-  {
-    public string path { get; set; }
-    public bool isdir { get; set; }
-    public string fileexten { get; set; }
-  }
   public class CurDirectory
   {
-    public string path { get; set; }
-    private List<string> directories { get; set; }
-    private List<string> files { get; set; }
-    public List<filitem> items { get; private set; }
+    public DirectoryInfo CurDir = new DirectoryInfo("c:\\");
+    public List<FileInfo> Files { get; private set; }
+    public List<DirectoryInfo> Dirs { get; private set; }
 
     public void getitems()
     {
-      getdir();
-      getfiles();
-      foreach (string i in directories)
-      {
-        filitem temp = new filitem();
-        temp.path = i;
-        temp.isdir = true;
-        items.Add(temp);
-      }
-      foreach (string i in files)
-      {
-        filitem temp = new filitem();
-        temp.path = i;
-        temp.isdir = false;
-        temp.fileexten = Path.GetExtension(temp.path);
-        items.Add(temp);
-      }
-    }
-
-    private void getdir()
-    {
-      this.directories = (Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly)).ToList();
-    }
-    private void getfiles()
-    {
-      this.files = (Directory.GetFiles(path)).ToList();
+      Files = CurDir.GetFiles().ToList();
+      Dirs = CurDir.GetDirectories().ToList();
     }
   }
 }
