e72319c2253afb0d072bbf197a6644af8d55064f
Tyler Brody <brody.tyler.46@gmail.com>
2020-08-15 17:31:07 -0700

    more things fixedrenamed and edited

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Materialfile.filereader;
 
@@ -7,16 +8,30 @@ namespace Materialfile
 {
   public class MainWindow : Window
   {
-    public CurDirectory cur { get; set; }
+    public CurDirectory cur = new CurDirectory();
     public MainWindow()
     {
-      cur = new CurDirectory();
-      cur.getitems();
+      cur.CurDir = new System.IO.DirectoryInfo(cur.homePath);
       InitializeComponent();
       DataContext = this;
 #if DEBUG
       this.AttachDevTools();
 #endif
+    }
+
+    private void ChangeFolder(object sender, RoutedEventArgs e)
+    {
+      cur.CurDir = new System.IO.DirectoryInfo(((Button)sender).Tag.ToString()+cur.OSslash);
+      System.Console.WriteLine(((Button)sender).Tag.ToString());
+    }
+
+    private void UpFolder()
+    {
+      if (System.IO.Directory.GetParent(cur.CurDir.FullName) != null) { cur.CurDir = System.IO.Directory.GetParent(cur.CurDir.FullName); System.Console.WriteLine(cur.CurDir.FullName);}
+      else
+      {
+        System.Console.WriteLine("At top");
+      }
     }
 
     private void InitializeComponent()
---
+++ Materialfile.filereaders/curDirectory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+namespace Materialfile.filereader
+{
+  public class CurDirectory : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    readonly public string homePath = (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX) ? Environment.GetEnvironmentVariable("HOME") : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+    
+    readonly public string OSslash = (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX) ? "/" : "\\";
+
+    private DirectoryInfo _CurDir;
+    public DirectoryInfo CurDir
+    { get { return _CurDir; } set {
+        _CurDir = value;
+        getitems();
+        OnPropertyChanged();
+      }  }
+
+    private List<FileInfo> _Files;
+    public List<FileInfo> Files
+    {
+      get { return _Files; }
+      set
+      {
+        _Files = value;
+        OnPropertyChanged();
+      }
+    }
+    private List<DirectoryInfo> _Dirs;
+    public List<DirectoryInfo> Dirs
+    {
+      get { return _Dirs; }
+      set
+      {
+        _Dirs = value;
+        OnPropertyChanged();
+      }
+    }
+
+    private void getitems()
+    {
+      Files = CurDir.GetFiles().ToList();
+      Dirs = CurDir.GetDirectories().ToList();
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+  }
+}
